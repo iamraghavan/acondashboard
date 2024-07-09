ef34d4ae3782a4a29abc18aecb382e2302cbb2b9
iam.raghavan <developer.raghavan@gmail.com>
2024-07-09 22:23:20 +0530

    chore: Update staticfiles configuration for production deployment

--- acondashboard/settings.py
+++ acondashboard/settings.py
@@ -141,8 +141,17 @@ STATICFILES_DIRS = [
 
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js'),
 
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_KEEP_ONLY_HASHED_FILES = False
+
 
 
 # Where collectstatic will copy files
